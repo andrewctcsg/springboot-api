ca182eaa6dfcf00a4be56dbf497cb13975af3379
andrewctcsg <56865859+andrewctcsg@users.noreply.github.com>
2020-01-22 11:48:05 +0800

    Update UserController.java

--- src/main/java/com/ctcglobal/mongodbtable/controller/UserController.java
+++ src/main/java/com/ctcglobal/mongodbtable/controller/UserController.java
@@ -25,10 +25,10 @@ public class UserController {
         return userRepository.findAll();
     }
 
-    @RequestMapping(value = "/name/{fname}", method = RequestMethod.GET)
-    public User getUser(@PathVariable String fname) {
-        LOG.info("Getting user with name: {}.", fname);
-        return userRepository.getUserByFname(fname);
+    @RequestMapping(value = "/name/{first_name}", method = RequestMethod.GET)
+    public User getUser(@PathVariable String first_name) {
+        LOG.info("Getting user with name: {}.", first_name);
+        return userRepository.getUserByFirst_name(first_name);
     }
 
     @RequestMapping(value = "/email/{email}", method = RequestMethod.GET)
