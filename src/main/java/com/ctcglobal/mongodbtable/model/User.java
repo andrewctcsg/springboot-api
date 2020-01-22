6b77b77a5fd36e0b61c5660e525397c92b6ce0d5
andrewctcsg <56865859+andrewctcsg@users.noreply.github.com>
2020-01-22 13:35:30 +0800

    Update User.java

--- src/main/java/com/ctcglobal/mongodbtable/model/User.java
+++ src/main/java/com/ctcglobal/mongodbtable/model/User.java
@@ -7,20 +7,20 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
     @Id
     private String email;
-    private String fname;
-    private String lname;
+    private String first_name;
+    private String last_name;
     private String mobile;
 
     public String getEmail() {
         return email;
     }
 
-    public String getFiname() {
-        return fname;
+    public String getFname() {
+        return first_name;
     }
 
     public String getLname() {
-        return lname;
+        return last_name;
     }
 
     public String getMobile() {
